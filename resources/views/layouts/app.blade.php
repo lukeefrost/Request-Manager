c8f1b57780690cdf19404f329ee918540ae2e81b
lukelliotfrost <luke@radio.co>
2020-05-22 14:17:41 +0100

    Added Guzzle client to the application and used get request to get contents of all items. Also used a resource controller for the get request. Also created index page to display all items.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Request Manager</title>
+  </head>
+  <body>
+    @include('inc.navbar')
+    @include('inc.messages')
+    @yield('content')
+  </body>
+</html>
